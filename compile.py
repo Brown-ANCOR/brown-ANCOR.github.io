77e94e948e54f2316330c6309087a58783f0331a
aalok-sathe <aalok@brown.edu>
2025-10-03 15:35:35 -0400

    make two columns one for past one for upcoming

--- compile.py
+++ compile.py
@@ -2,6 +2,7 @@
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 
 # Load YAML files
 with open("static/data/talks.yml", "r") as talks_file:
@@ -15,7 +16,9 @@ env = Environment(loader=FileSystemLoader("."))
 template = env.get_template("index.template.html")
 
 # Render the template with the data from YAML files
-rendered_html = template.render(talks=talks, info=info)
+rendered_html = template.render(
+    talks=talks, info=info, current_date=datetime.now().date()
+)
 
 # Write the rendered HTML to index.html
 with open("index.html", "w") as output_file:
